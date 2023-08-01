fdcf7b66a0bcc16b833b2863a78f6f23b2c998a2
ProvLeon <providence.leonard@gmail.com>
2023-08-01 18:14:31 +0100

    updated portfolio info

--- data.js
+++ data.js
@@ -2,6 +2,6 @@ export const heading = [
   {
     title: 'Welcome to\nMy Personal Portfolio',
     description:
-      'The Best Website to be, an expert in HTML, CSS, and JavaScript',
+      'A Software Engineer experienced building and debugging software applications. With Expertise in Python, JavaScript and HTML. Have proven ability to troubleshoot and identify solutions quickly and efficiently. With much saying, Lets just Code!',
   },
 ];
